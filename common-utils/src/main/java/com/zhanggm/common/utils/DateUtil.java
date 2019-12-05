f5b572fa580de1c3665333a26dfc3b1dea2df308
zhanggm <zhanggm1002@qq.com>
2019-12-05 11:03:22 +0800

    修改日期和字符串工具类

--- common-utils/src/main/java/com/zhanggm/common/utils/DateUtil.java
+++ common-utils/src/main/java/com/zhanggm/common/utils/DateUtil.java
@@ -1,5 +1,56 @@
 package com.zhanggm.common.utils;
 
-public class DateUtil {
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
 
+public class DateUtil {
+	/**
+	 *   根据生日计算年龄
+	 * @param birthDate
+	 * @return
+	 */
+	public static int getAge(Date birthDate) {
+		//获得日历控件
+		Calendar calendar = Calendar.getInstance();
+		int nowYear = calendar.get(Calendar.YEAR);
+		int nowMonth = calendar.get(Calendar.MONTH);
+		int nowDay = calendar.get(Calendar.DAY_OF_MONTH);
+		//设置生日
+		calendar.setTime(birthDate);
+		int birthYear = calendar.get(Calendar.YEAR);
+		int birthMonth = calendar.get(Calendar.MONTH);
+		int birthDay = calendar.get(Calendar.DAY_OF_MONTH);
+		//年龄
+		int age = nowYear-birthYear;
+		//如果生日的月份大于当前月份时，年龄-1
+		if(birthMonth>nowMonth) {
+			age--;
+		}
+		//如果月份相等，判断日期
+		if(birthMonth==nowMonth && nowDay<birthDay) {
+			age--;
+		}
+		return age;
+	}
+	/**
+	 * 根据出生日期计算年龄
+	 * @param birthDateStr "2019-11-08"
+	 * @return
+	 */
+	public static int getAge(String birthDateStr) {
+		SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd");
+		Date birthDate = null;
+		try {
+			birthDate = simpleDateFormat.parse(birthDateStr);
+		} catch (ParseException e) {
+			e.printStackTrace();
+		}
+		return getAge(birthDate);
+	}
+	
+	public static void main(String[] args) {
+		System.out.println(getAge("1980-02-30"));
+	}
 }
--- common-utils/src/main/java/com/zhanggm/common/utils/StringUtil.java
+++ common-utils/src/main/java/com/zhanggm/common/utils/StringUtil.java
@@ -1,15 +1,41 @@
 package com.zhanggm.common.utils;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class StringUtil {
+	/**
+	 * @Title: isBlank   
+	 * @Description: 判断字符串是否为空  
+	 * @param: @param str
+	 * @param: @return      
+	 * @return: boolean      
+	 * @throws
+	 */
 	public static boolean isBlank(String str) {
-		if(str!=null) {
-			str.trim();
-			System.out.println(str);
+		if(str==null) {
+			return false;
+		}
+		//去空格
+		str = str.trim();
+		//
+		if(str.length()==0) {
+			return false;
 		}
 		return true;
 	}
+	/**
+	 * 判断字符串是否为手机号
+	 * @param str
+	 * @return
+	 */
+	public static boolean isPhoneNum(String str) {
+		String regex = "1[3578]\\d{9}";
+		return str.matches(regex);
+	}
+	
 	
 	public static void main(String[] args) {
-		isBlank(" ");
+		System.out.println(isPhoneNum("1818106007"));
 	}
 }
