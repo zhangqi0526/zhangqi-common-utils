7e539dd75d4d684990411914df3a372c3feb8ffc
zhanggm <zhanggm1002@qq.com>
2019-12-05 10:17:19 +0800

    修改StringUtil类

--- common-utils/src/main/java/com/zhanggm/common/utils/StringUtil.java
+++ common-utils/src/main/java/com/zhanggm/common/utils/StringUtil.java
@@ -1,5 +1,15 @@
 package com.zhanggm.common.utils;
 
 public class StringUtil {
-
+	public static boolean isBlank(String str) {
+		if(str!=null) {
+			str.trim();
+			System.out.println(str);
+		}
+		return true;
+	}
+	
+	public static void main(String[] args) {
+		isBlank(" ");
+	}
 }
