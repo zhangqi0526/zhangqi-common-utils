c75380de02ec14588e40a9fda0890d121d5683e5
zhanggm <zhanggm1002@qq.com>
2019-12-06 11:32:38 +0800

    添加测试类、编译插件、源码插件

---
+++ common-utils/src/test/java/com/zhanggm/common/utils/test/DateUtilTest.java
@@ -0,0 +1,12 @@
+package com.zhanggm.common.utils.test;
+
+import org.junit.Test;
+
+import com.zhanggm.common.utils.DateUtil;
+
+public class DateUtilTest {
+	@Test
+	public void test() {
+		System.out.println(DateUtil.getAge("2018-10-24"));
+	}
+}
