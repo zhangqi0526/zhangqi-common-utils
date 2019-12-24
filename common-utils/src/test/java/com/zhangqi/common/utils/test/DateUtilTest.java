d417f5a36c024aa3985aa0321501f0d8a73b362a
zhangqidage <1807109727@qq.com>
2019-12-24 18:39:02 +0800

    完善工具类

---
+++ common-utils/src/test/java/com/zhangqi/common/utils/test/DateUtilTest.java
@@ -0,0 +1,12 @@
+package com.zhangqi.common.utils.test;
+
+import org.junit.Test;
+
+import com.zhangqi.common.utils.DateUtil;
+
+public class DateUtilTest {
+	@Test
+	public void test() {
+		System.out.println(DateUtil.getAge("2018-10-24"));
+	}
+}
